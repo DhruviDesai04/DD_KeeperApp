0a0bd8e9bafdd7a1ed0b06c034d58fe54b5e02b9
Dhruvi Desai <dhruvidesai4402@gmail.com>
2021-07-31 19:08:14 +0530

    Updates

---
+++ precache-manifest.b11a6a07b7fb4dc878e0c6d3535debb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5b61b50444c7cc4c2a6e696c93d66d",
+    "url": "/DD_keeperApp/index.html"
+  },
+  {
+    "revision": "7810d98208fcac93d282",
+    "url": "/DD_keeperApp/static/js/2.11caf667.chunk.js"
+  },
+  {
+    "revision": "e33915eaf77dee4127d4",
+    "url": "/DD_keeperApp/static/js/main.b5062a2f.chunk.js"
+  },
+  {
+    "revision": "187fa4306509d228ce98",
+    "url": "/DD_keeperApp/static/js/runtime-main.7d163dab.js"
+  }
+]);
